aa6bbe41a426a4c0e3d3a44fe2917345a4f09b20
Nick Escalona <nicholas.escalona@gmail.com>
2020-04-28 15:52:27 -0500

    point to the dockerized api.

--- ng-notes-client/src/environments/environment.prod.ts
+++ ng-notes-client/src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  notesApiBaseUrl: 'https://2002-notes-api.azurewebsites.net/'
+  // notesApiBaseUrl: 'https://2002-notes-api.azurewebsites.net/'
+  notesApiBaseUrl: 'https://2002-notes-api-docker.azurewebsites.net/'
 };
